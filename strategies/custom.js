3162d9a07e5d069725b1803eaea5356b85fe12e8
Marcus Lopes <marcusporteira@gmail.com>
2017-07-09 18:38:40 -0300

    Created custom strategy mixing MACD, RSI and UO

--- strategies/custom.js
+++ strategies/custom.js
@@ -1,62 +1,160 @@
-// This is a basic example strategy for Gekko.
-// For more information on everything please refer
-// to this document:
-//
-// https://gekko.wizb.it/docs/strategies/creating_a_strategy.html
-//
-// The example below is pretty bad investment advice: on every new candle there is
-// a 10% chance it will recommend to change your position (to either
-// long or short).
+/*
+  
+  MACD + RSI- By Marcus Lopes
+  Obs: Trend verificada somente para o MACD
 
-var log = require('../core/log');
+  
 
-// Let's create our own strat
-var strat = {};
+ */
 
-// Prepare everything our method needs
-strat.init = function() {
-  this.currentTrend = 'long';
-  this.requiredHistory = 0;
+// helpers
+var _ = require('lodash');
+var log = require('../core/log.js');
+
+// configuration
+var config = require('../core/util.js').getConfig();
+var macdSettings = config.MACD;
+var rsiSettings = config.RSI;
+var uoSettings = config.UO;
+
+// let's create our own method
+var method = {};
+
+// prepare everything our method needs
+method.init = function() {
+  this.name = 'MACD&RSI';
+  // keep state about the current trend
+  // here, on every new candle we use this
+  // state object to check if we need to
+  // report it.
+  this.trend = {
+    direction: 'none',
+    duration: 0,
+    persisted: false,
+    adviced: false
+  };
+
+  // how many candles do we need as a base
+  // before we can start giving advice?
+  this.requiredHistory = config.tradingAdvisor.historySize;
+
+  // define the indicators we need
+  this.addIndicator('macd', 'MACD', macdSettings);
+  this.addIndicator('rsi', 'RSI', rsiSettings);
+  this.addIndicator('uo', 'UO', uoSettings);
 }
 
-// What happens on every new candle?
-strat.update = function(candle) {
-
-  // Get a random number between 0 and 1.
-  this.randomNumber = Math.random();
-
-  // There is a 10% chance it is smaller than 0.1
-  this.toUpdate = this.randomNumber < 0.1;
+// what happens on every new candle?
+method.update = function(candle) {
+  // nothing!
 }
 
-// For debugging purposes.
-strat.log = function() {
-  log.debug('calculated random number:');
-  log.debug('\t', this.randomNumber.toFixed(3));
+// for debugging purposes: log the last calculated
+// EMAs and diff.
+method.log = function() {
+  var digits = 8;
+  var macd = this.indicators.macd;
+  var rsi = this.indicators.rsi;
+  var uo = this.indicators.uo;
+
+  var diff = macd.diff;
+  var signal = macd.signal.result;
+
+  log.debug('calculated MACD properties for candle:');
+  log.debug('\t', 'short:', macd.short.result.toFixed(digits));
+  log.debug('\t', 'long:', macd.long.result.toFixed(digits));
+  log.debug('\t', 'macd:', diff.toFixed(digits));
+  log.debug('\t', 'signal:', signal.toFixed(digits));
+  log.debug('\t', 'macdiff:', macd.result.toFixed(digits));
+  log.debug('calculated RSI properties for candle:');
+  log.debug('\t', 'rsi:', rsi.rsi.toFixed(digits));
+  log.debug('\t', 'price:', candle.close.toFixed(digits));
+  log.debug('calculated Ultimate Oscillator properties for candle:');
+  log.debug('\t', 'UO:', uo.uo.toFixed(digits));
+  log.debug('\t', 'price:', candle.close.toFixed(digits));
 }
 
-// Based on the newly calculated
-// information, check if we should
-// update or not.
-strat.check = function() {
+method.check = function() {
+  var macddiff = this.indicators.macd.result;
+  var rsi = this.indicators.rsi;
+  var rsiVal = rsi.rsi;
+  var uo = this.indicators.uo;
+  var uoVal = uo.uo;
 
-  // Only continue if we have a new update.
-  if(!this.toUpdate)
-    return;
+  if(macddiff > macdSettings.thresholds.up && 
+    (rsiVal > rsiSettings.thresholds.high ) && 
+    (uoVal > uoSettings.thresholds.high)) {
 
-  if(this.currentTrend === 'long') {
+    // new trend detected
+    if(this.trend.direction !== 'up')
+      // reset the state for the new trend
+      this.trend = {
+        duration: 0,
+        persisted: false,
+        direction: 'up',
+        adviced: false
+      };
 
-    // If it was long, set it to short
-    this.currentTrend = 'short';
-    this.advice('short');
+    this.trend.duration++;
+
+    log.debug('In uptrend since', this.trend.duration, 'candle(s)');
+
+    if(this.trend.duration >= macdSettings.thresholds.persistence)
+      this.trend.persisted = true;
+
+    if(this.trend.persisted && !this.trend.adviced) {
+      this.trend.adviced = true;
+      this.advice('long');
+    } else
+      this.advice();
+
+  } else if(macddiff < macdSettings.thresholds.down && 
+            (rsiVal < rsiSettings.thresholds.low ) &&
+             (uoVal < uoSettings.thresholds.low) ) {
+
+    // new trend detected
+    if(this.trend.direction !== 'down')
+      // reset the state for the new trend
+      this.trend = {
+        duration: 0,
+        persisted: false,
+        direction: 'down',
+        adviced: false
+      };
+
+    this.trend.duration++;
+
+    log.debug('In downtrend since', this.trend.duration, 'candle(s)');
+
+    if(this.trend.duration >= macdSettings.thresholds.persistence)
+      this.trend.persisted = true;
+
+    if(this.trend.persisted && !this.trend.adviced) {
+      this.trend.adviced = true;
+      this.advice('short');
+    } else
+      this.advice();
 
   } else {
 
-    // If it was short, set it to long
-    this.currentTrend = 'long';
-    this.advice('long');
+    log.debug('In no trend');
 
+    // we're not in an up nor in a downtrend
+    // but for now we ignore sideways trends
+    // 
+    // read more @link:
+    // 
+    // https://github.com/askmike/gekko/issues/171
+
+    // this.trend = {
+    //   direction: 'none',
+    //   duration: 0,
+    //   persisted: false,
+    //   adviced: false
+    // };
+
+    this.advice();
   }
 }
 
-module.exports = strat;
+module.exports = method;
